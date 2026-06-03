d8fb90c82a41e2f862b3103f9a4fdbee9171a02c
sh <16303791+speedyhoon@users.noreply.github.com>
2026-06-03 16:50:44 +1000

    Fix issue with ToBGRX32 sending padding byte as 0x00 instead of 0xFF. Add ToBGR032()

--- bytes.go
+++ bytes.go
@@ -6,8 +6,14 @@ func (c C32) ToBGRA32() []byte {
 	return []byte{byte(c >> 8), byte(c >> 16), byte(c >> 24), byte(c)}
 }
 
-// ToBGRX32 uses 32 bits per pixel with 8 bits per component
-// with the order Blue, Green, Red, and a fourth padding byte when transparency is not required.
-func (c C32) ToBGRX32() []byte {
+// ToBGR032 packs a pixel into 32-bit BGR format (B, G, R, 8-bit padding),
+// where the padding byte is always set to 0x00.
+func (c C32) ToBGR032() []byte {
 	return []byte{byte(c >> 8), byte(c >> 16), byte(c >> 24), 0}
+}
+
+// ToBGRX32 packs a pixel into 32-bit BGRX format (B, G, R, 8-bit padding),
+// where the padding byte is always set to 0xFF.
+func (c C32) ToBGRX32() []byte {
+	return []byte{byte(c >> 8), byte(c >> 16), byte(c >> 24), 255}
 }
